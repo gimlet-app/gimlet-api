d98f97bd5a2b8903730e82acc2ebe4e43860abd2
Eric Larson <ewlarson@gmail.com>
2021-03-26 09:39:36 -0500

    Update questions_list_json.php
    {$var}

--- php/questions_list_json.php
+++ php/questions_list_json.php
@@ -4,10 +4,10 @@ require './vars.php';
 $thirty_days_ago = date('Y-m-d', strtotime('-30 days'));
 $today = date('Y-m-d');
 
-$api_url = "https://${DOMAIN}.gimlet.us/api/v1/sites/${SITE_ID}/questions?start_date=${thirty_days_ago}&end_date=${today}";
+$api_url = "https://{$DOMAIN}.gimlet.us/api/v1/sites/{$SITE_ID}/questions?start_date={$thirty_days_ago}&end_date={$today}";
 
 $req = curl_init($api_url);
-$credentials = "${EMAIL}:${API_KEY}";
+$credentials = "{$EMAIL}:{$API_KEY}";
 curl_setopt($req, CURLOPT_USERPWD, $credentials);
 curl_setopt($req, CURLOPT_RETURNTRANSFER, true);
 
